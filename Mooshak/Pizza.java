a62a565cfffb637271381bf24beed047a5c33938
Fabio <127618255+FabioSemedo@users.noreply.github.com>
2024-10-22 19:56:44 +0100

    Working Pizza

--- Mooshak/Pizza.java
+++ Mooshak/Pizza.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Pizza {
-    public static boolean arrContains(int [] arr, int x){
-        for(int i =0; i< arr.length(); i++){
+    public static boolean arrContains(int [] arr, int size, int x){
+        for(int i =0; i< size; i++){
             if(arr[i]==x){
                 return true;
             }
@@ -14,33 +14,38 @@ public class Pizza {
         Scanner input = new Scanner(System.in);
         Scanner line;
         
-        int no_like[] = int[100];
+        int no_like[] = new int[100];
         int size = 0;
-        int pizzas;
-        int ingrid;
         int like = 0;
+        int pizzas;
+        int ingrid_num;
+        int flag;
 
         line = new Scanner(input.nextLine());
+        size = line.nextInt();
 
-        while(line.hasNextInt()){
-            no_like[size] = line.nextInt();
-            size++;
+        for(int i=0; i< size; i++){
+            no_like[i] = line.nextInt();
         }
 
         pizzas = Integer.parseInt(input.nextLine());
 
         for(int i= 0; i < pizzas; i++){
             line = new Scanner(input.nextLine());
-            ingrid = line.nextInt();
+            ingrid_num = line.nextInt();
+            flag = 1;
 
-            for(int k = 0; k< ingrid; k++){
-                if(arrContains(no_like, line.nextInt())){
-
+            for(int k = 0; k< ingrid_num; k++){
+                if( arrContains( no_like, size, line.nextInt())){
+                    flag=0;
                 }
+            }
+            if(flag == 1){
+                like++;
             }
         }
 
-
-        System.out.println(n);
+        input.close();
+        System.out.println(like);
     } 
 }
