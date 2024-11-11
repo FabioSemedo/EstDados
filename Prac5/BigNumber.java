5dd948d3374ed55145173f4d674763fc8cd9e65d
Fabio <127618255+FabioSemedo@users.noreply.github.com>
2024-11-11 14:03:04 +0000

    Big Numbers

--- Prac5/BigNumber.java
+++ Prac5/BigNumber.java
@@ -1,11 +1,51 @@
 public class BigNumber{
-    private String num;
+    private int num[];
 
     BigNumber(String n){
-        num = n;
+        int num[] = new int[ n.length()];
+
+        for(i=0; i< n.length();i++){
+            num[i]=n.charAt(i);
+        }
     }
 
-    
+    public BigInteger getLen() {
+        return num.length;
+    }
 
-    
+    public int[] getNum() {
+        return num;
+    }
+
+    public boolean equals(BigNumber n){
+        int i=0; 
+        int len = this.getLen();
+
+        if(n.getLen()!= len) 
+            return false;
+        
+        num2 = n.getNum();
+
+        while(i<len){
+            if(this.num[i] != num2[i]) 
+                return false;
+            i++;
+        }
+
+        return true;
+    }
+
+    public String toString(){
+        String s = "";
+        for (int i : this.num) {
+            s += i;
+        }
+        return s;
+    }
+
+    public BigNumber add(BigNumber n){
+        
+        BigNumber res = new
+    }
+
 }
